49c16a71c515291715d4a61ffdf99b36f6f050d6
Romain GRANDJEAN <romaingrandjean@Romains-MacBook-Air.local>
2020-07-24 10:56:55 +0200

    Updates

---
+++ precache-manifest.f04980cfd90dfe346db1419dc967a81e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1244000a27eececd294b338a4a876fb",
+    "url": "/code-natives/index.html"
+  },
+  {
+    "revision": "0c35ad8d9a3ec00ff424",
+    "url": "/code-natives/static/css/main.a0bc7eca.chunk.css"
+  },
+  {
+    "revision": "ff3a65543f5dfaed6c92",
+    "url": "/code-natives/static/js/2.33d57399.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/code-natives/static/js/2.33d57399.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c35ad8d9a3ec00ff424",
+    "url": "/code-natives/static/js/main.fff7d635.chunk.js"
+  },
+  {
+    "revision": "ce97494664d52bfabdf3",
+    "url": "/code-natives/static/js/runtime-main.204ed712.js"
+  }
+]);
