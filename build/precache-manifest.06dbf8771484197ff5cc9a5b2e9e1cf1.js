129b9da1152a40669c88ee96a78f63b25bfe670a
Romain GRANDJEAN <romaingrandjean@Romains-MacBook-Air.local>
2020-07-24 12:06:59 +0200

    small fix

---
+++ build/precache-manifest.06dbf8771484197ff5cc9a5b2e9e1cf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b33dee3bc676dfa7af171a22fd6e71ad",
+    "url": "/codenatives-beta/index.html"
+  },
+  {
+    "revision": "dc1552c1dcf6da8f5ffe",
+    "url": "/codenatives-beta/static/css/main.a0bc7eca.chunk.css"
+  },
+  {
+    "revision": "0259e2c682197ac90536",
+    "url": "/codenatives-beta/static/js/2.f942d9c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/codenatives-beta/static/js/2.f942d9c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc1552c1dcf6da8f5ffe",
+    "url": "/codenatives-beta/static/js/main.0da66d90.chunk.js"
+  },
+  {
+    "revision": "b52e91f076e7a880cdab",
+    "url": "/codenatives-beta/static/js/runtime-main.89343d86.js"
+  }
+]);
