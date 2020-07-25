725b15d2c7187145b33dbfb470ad997d711acff0
Romain GRANDJEAN <romaingrandjean@Romains-MacBook-Air.local>
2020-07-25 08:34:48 +0200

    Updates

--- precache-manifest.f9907c7e9fcea903c6a94ba4a1604eb6.js
+++ precache-manifest.f9907c7e9fcea903c6a94ba4a1604eb6.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b33dee3bc676dfa7af171a22fd6e71ad",
+    "revision": "18d427b48e024aca47836aba426151fc",
     "url": "/codenatives-beta/index.html"
   },
   {
-    "revision": "dc1552c1dcf6da8f5ffe",
+    "revision": "f87b2b382271649f8598",
     "url": "/codenatives-beta/static/css/main.a0bc7eca.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/codenatives-beta/static/js/2.f942d9c4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "dc1552c1dcf6da8f5ffe",
-    "url": "/codenatives-beta/static/js/main.0da66d90.chunk.js"
+    "revision": "f87b2b382271649f8598",
+    "url": "/codenatives-beta/static/js/main.e5c825bc.chunk.js"
   },
   {
     "revision": "b52e91f076e7a880cdab",
