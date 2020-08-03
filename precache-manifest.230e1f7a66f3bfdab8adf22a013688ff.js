d04828f7835b14b16a7fee4785e9f11de8bfacf0
Romain GRANDJEAN <romaingrandjean@Romains-MacBook-Air.local>
2020-08-03 14:25:12 +0200

    Updates

--- precache-manifest.230e1f7a66f3bfdab8adf22a013688ff.js
+++ precache-manifest.230e1f7a66f3bfdab8adf22a013688ff.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "616fe04af9edb536361b494855331947",
+    "revision": "9ae997d7346c19f429e2727291de7979",
     "url": "/codenatives-beta/index.html"
   },
   {
-    "revision": "60003bea3b158c85670a",
+    "revision": "4679ad2a206c8d8b4784",
     "url": "/codenatives-beta/static/css/main.10e0383d.chunk.css"
   },
   {
-    "revision": "ff3a65543f5dfaed6c92",
-    "url": "/codenatives-beta/static/js/2.33d57399.chunk.js"
+    "revision": "9dd76e15c9830408b273",
+    "url": "/codenatives-beta/static/js/2.407bf550.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/codenatives-beta/static/js/2.33d57399.chunk.js.LICENSE.txt"
+    "url": "/codenatives-beta/static/js/2.407bf550.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "60003bea3b158c85670a",
-    "url": "/codenatives-beta/static/js/main.d907640d.chunk.js"
+    "revision": "4679ad2a206c8d8b4784",
+    "url": "/codenatives-beta/static/js/main.a1c021ee.chunk.js"
   },
   {
     "revision": "b52e91f076e7a880cdab",
