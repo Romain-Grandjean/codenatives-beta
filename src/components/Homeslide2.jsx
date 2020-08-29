02a58bcc3f262713a38d9c33cd0f5052c82a4a73
in-roma <romain.grandjean@outlook.com>
2020-08-29 09:39:18 +0200

    Language selection component

--- src/components/Homeslide2.jsx
+++ src/components/Homeslide2.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import BtnLanguage from './buttons/BtnLanguage';
 
 const Homeslide2 = () => {
 	return (
@@ -12,15 +13,7 @@ const Homeslide2 = () => {
 					<span id="language-catchphrase">Choose your language</span>
 					<div className="btn-language">
 						<img id="language-arrow" src={process.env.PUBLIC_URL + '/img/arrow-btn-selector.svg'} alt="" />
-						<div className="language-selection">
-							<span>Javascript</span>
-							<span>HTML/CSS</span>
-							<span>React</span>
-							<span>Node JS</span>
-							<span>Command lines</span>
-							<span>Git / Github</span>
-							<span>Full stack</span>
-						</div>
+						<BtnLanguage />
 					</div>
 				</div>
 				<div className="central-buttons">
---
+++ src/components/buttons/BtnLanguage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Startwindow = (props) => {
+
+
+	return (
+		<div className="language-selection">
+							<a>Javascript</a>
+							<a>HTML/CSS</a>
+							<a>React</a>
+							<a>Node JS</a>
+							<a>Command lines</a>
+							<a>Git / Github</a>
+							<a>Full stack</a>
+						</div>
+	);
+};
+
+export default Startwindow;
