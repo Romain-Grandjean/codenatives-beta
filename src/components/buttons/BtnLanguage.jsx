744da1c91dd6a68767be69f718e53c079e9a4b81
in-roma <romain.grandjean@outlook.com>
2020-08-29 15:48:26 +0200

    responsive inputs

--- src/components/buttons/BtnLanguage.jsx
+++ src/components/buttons/BtnLanguage.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 
-const Startwindow = (props) => {
-
-
+const BtnLanguage = (props) => {
 	return (
 		<div className="language-selection">
-							<a>Javascript</a>
-							<a>HTML/CSS</a>
-							<a>React</a>
-							<a>Node JS</a>
-							<a>Command lines</a>
-							<a>Git / Github</a>
-							<a>Full stack</a>
-						</div>
+			<a>Javascript</a>
+			<a>HTML/CSS</a>
+			<a>React</a>
+			<a>Node JS</a>
+			<a>Command lines</a>
+			<a>Git / Github</a>
+			<a>Full stack</a>
+		</div>
 	);
 };
 
-export default Startwindow;
+export default BtnLanguage;
