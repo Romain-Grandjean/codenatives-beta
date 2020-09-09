f9ce81d6b0677aff68d33a80e681cc4f773be8d0
in-roma <romain.grandjean@outlook.com>
2020-09-09 21:41:18 +0200

    Updates

---
+++ precache-manifest.046563cca61f2d7f11ba4c0740be0d97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "036060d5b131b59c23446d761fec91f7",
+    "url": "/codenatives-beta/index.html"
+  },
+  {
+    "revision": "8530d2c2e4d50878ac18",
+    "url": "/codenatives-beta/static/css/main.3bd36e92.chunk.css"
+  },
+  {
+    "revision": "b0a0352a8f70177c0a22",
+    "url": "/codenatives-beta/static/js/2.d41142a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/codenatives-beta/static/js/2.d41142a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8530d2c2e4d50878ac18",
+    "url": "/codenatives-beta/static/js/main.e4963902.chunk.js"
+  },
+  {
+    "revision": "b52e91f076e7a880cdab",
+    "url": "/codenatives-beta/static/js/runtime-main.89343d86.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/codenatives-beta/precache-manifest.f3679ebaa75b2ed7cc44831343ee5013.js"
+  "/codenatives-beta/precache-manifest.046563cca61f2d7f11ba4c0740be0d97.js"
 );
 
 self.addEventListener('message', (event) => {
