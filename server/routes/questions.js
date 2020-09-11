5a2cd1aff69a818733babde2860a01eaf4e8be0a
in-roma <romain.grandjean@outlook.com>
2020-09-11 22:55:47 +0200

    server addons/routes working

--- server/routes/questions.js
+++ server/routes/questions.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
+// Get all questions
 router.get('/', async (req, res) => {
 	try {
 		const questions = await Question.find();
@@ -13,5 +14,17 @@ router.get('/', async (req, res) => {
 		});
 	}
 });
+// Get one specific question
+router.get('/:id'),
+	async (req, res) => {
+		try {
+			const question = Question.findById(req.params.id);
+			res.send(question);
+		} catch (error) {
+			return res
+				.status(404)
+				.send('The customer with the given ID was not found.');
+		}
+	};
 
 module.exports = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,15 +3,29 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
+// Get all users
 router.get('/', async (req, res) => {
 	try {
-		const questions = await User.find();
-		res.send(questions);
+		const users = await User.find();
+		res.send(users);
 	} catch (err) {
 		res.status(500).json({
 			message: err.message,
 		});
 	}
 });
+
+// Get one specific user
+router.get('/:id'),
+	async (req, res) => {
+		try {
+			const user = User.findById(req.params.id);
+			res.send(user);
+		} catch (error) {
+			return res
+				.status(404)
+				.send('The customer with the given ID was not found.');
+		}
+	};
 
 module.exports = router;
