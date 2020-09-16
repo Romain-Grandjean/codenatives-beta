cdf1e426c5aad7c7b8b0340d9126a063d2b80b22
in-roma <romain.grandjean@outlook.com>
2020-09-16 11:40:05 +0200

    setting up a keygenerator

--- client/src/components/admin/tableBody.jsx
+++ client/src/components/admin/tableBody.jsx
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
 
 const TableBody = (props) => {
+	const createKey = (item) => {
+		let indexEle = props.data.indexOf(item);
+
+		return indexEle;
+	};
+
 	return (
 		<tbody>
 			{props.data.map((item) => (
-				<tr>
-					<td>{item._id}</td>
-					<td>{item.level}</td>
-					<td>{item.type}</td>
-					<td>{item.question}</td>
-					<td >
-						<button className="btn-small btn-nocolor">Edit</button>
+				<tr id="tr" key={createKey(item)} className="row">
+					<td key={createKey(item)} className="td1">
+						{item._id}
 					</td>
-					<td >
-						<button className="btn-small btn-red">Delete</button>
+					<td key={createKey(item)} className="td2">
+						{item.level}
+					</td>
+					<td key={createKey(item)} className="td3">
+						{item.type}
+					</td>
+					<td key={createKey(item)} className="td4">
+						{item.question}
+					</td>
+					<td>
+						<button
+							key={createKey(item)}
+							className="btn-small btn-nocolor"
+						>
+							Edit
+						</button>
+					</td>
+					<td>
+						<button
+							key={createKey(item)}
+							className="btn-small btn-red"
+						>
+							Delete
+						</button>
 					</td>
 				</tr>
 			))}
