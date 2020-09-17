c226ec0572a037c9f279a72df9d701cf19c39305
in-roma <romain.grandjean@outlook.com>
2020-09-18 00:25:23 +0200

    update edit question

--- client/src/components/admin/editQuestion.jsx
+++ client/src/components/admin/editQuestion.jsx
@@ -17,16 +17,49 @@ class EditQuestion extends Component {
 		this.setState({ question });
 		console.log('this is state questions', this.state.question);
 	}
+	displaySolutions = () => {
+		const solutions = this.state.solutions;
+		return solutions.map((e) => e);
+	};
 
 	render() {
 		return (
 			<>
 				<div className="admin-container">
-					<form className="table-questions">
-						<label>Question</label>
-						<input label="Question" type="text">
-							<textarea>{this.state.question.question}</textarea>
-						</input>
+					<form className="admin-edit">
+						<label for="id">ID: </label>
+						<input
+							type="text"
+							name="id"
+							id="id"
+							required
+							readOnly
+							value={this.props.match.params.id}
+						></input>
+						<label for="question">Question: </label>
+						<input
+							type="text"
+							name="question"
+							id="question"
+							required
+							value={this.state.question.question}
+						></input>
+						<label for="answer">Solutions: </label>
+						<input
+							type="text"
+							name="answer"
+							id="answer"
+							required
+							value={this.displaySolutions}
+						></input>
+						<label for="answer">Answer: </label>
+						<input
+							type="text"
+							name="answer"
+							id="answer"
+							required
+							value={this.state.question.answer}
+						></input>
 					</form>
 				</div>
 				<Footer />
