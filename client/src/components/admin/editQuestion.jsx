7d28b28cb28f27e4f2410a211e0cd5c8b8ec7710
in-roma <romain.grandjean@outlook.com>
2020-09-20 22:11:14 +0200

    inserting btn edit page

--- client/src/components/admin/editQuestion.jsx
+++ client/src/components/admin/editQuestion.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Footer from '../structure/Footer';
-import { getOneElement } from '../../services/elementsService';
+import { getOneElement, deleteElement } from '../../services/elementsService';
+import { toast } from 'react-toastify';
 
 class EditQuestion extends Component {
 	constructor(props) {
@@ -18,9 +19,19 @@ class EditQuestion extends Component {
 
 		delete question.__v;
 		delete question._id;
+		delete question.type;
 		this.setState({ question });
 		console.log('this is state question', this.state.question);
 	}
+
+	handleDelete = async (element) => {
+		try {
+			await deleteElement(element);
+		} catch (error) {
+			if (error.response && error.response.status === 404)
+				toast.error('This element has already been deleted');
+		}
+	};
 
 	render() {
 		return (
@@ -28,9 +39,7 @@ class EditQuestion extends Component {
 				<div className="admin-container">
 					<form className="admin-edit">
 						<h1>Question ID: {this.props.match.params.id}</h1>
-						{
-						Object.keys(this.state.question).map((key) => (
-							
+						{Object.keys(this.state.question).map((key) => (
 							<div>
 								<label for={key}>{key}</label>
 								<input
@@ -41,8 +50,20 @@ class EditQuestion extends Component {
 									value={this.state.question[key]}
 								></input>
 							</div>
-						
 						))}
+						<div className="btn-zone">
+							<button
+								className="btn-small btn-red btn-padding"
+								onClick={() =>
+									this.onDelete(this.props.match.params.id)
+								}
+							>
+								Delete
+							</button>
+							<button className="btn-small btn-red btn-padding">
+								Edit
+							</button>
+						</div>
 					</form>
 				</div>
 				<Footer />
