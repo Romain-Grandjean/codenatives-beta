c9dfc0957d2e31ef27cab0dbf2a3cbf7da45ba1f
in-roma <romain.grandjean@outlook.com>
2020-09-27 00:29:21 +0200

    layout login form

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 
 export default function Login() {
-    return (
-        <div>
-            <form className="login-window">
-                <label>Email Address</label>
-                <input></input>
-                <label>Password</label>
-                <input></input>
-                <button>Login</button>
-                <button>Forgotten password</button>
-            </form>
+	return (
+		<div>
+			<form className="login-window">
+				<h1>Login</h1>
+				<div className="login-email">
+					<label>Email:</label>
+					<input></input>
+				</div>
+				<div className="login-password">
+					<label>Password:</label>
+					<input></input>
+				</div>
 
-        </div>
-    )
+				<button>You already have an account</button>
+
+				<button>Forgotten password</button>
+			</form>
+		</div>
+	);
 }
