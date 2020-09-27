341048bcc0ed175240fe29d9e8c53803343f6b79
in-roma <romain.grandjean@outlook.com>
2020-09-27 14:24:25 +0200

    login layout almost done

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -4,15 +4,23 @@ export default function Login() {
 	return (
 		<div>
 			<form className="login-window">
-				<h1 id='login-title'>Login</h1>
+				<h1 id="login-title">Login</h1>
 				<div className="login-email">
 					<label>Email :</label>
-					<input></input>
+					<div className="login-field">
+						<input autoFocus></input>
+						<div className="input-underline"></div>
+					</div>
 				</div>
 				<div className="login-password">
 					<label>Password :</label>
-					<input></input>
+					<div className="login-field">
+						<input></input>
+						<div className="input-underline"></div>
+
+					</div>
 				</div>
+				<button id="login-button" className="btn-big, btn-yellow">Login</button>
 
 				<button id="login-account">You already have an account</button>
 
--- client/src/components/login/Register.jsx
+++ client/src/components/login/Register.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 
 export default function Register() {
-    return (
-        <div>
-            <form className="login-window">
-                <label>First Name</label>
-                <input></input>
-                <label>Last Name</label>
-                <input></input>
-                <label>Email Address</label>
-                <input></input>
-                <button>Create account</button>
-            </form>
+	return (
+		<div>
+			<form className="login-window">
+				<label>First Name</label>
+				<input> autoFocus
+					{' '}
+			
+				</input>
+                <div className="input-underline"></div>
+				<label>Last Name</label>
+				<input>
+					<div className="input-underline"></div>
+				</input>
 
-        </div>
-    )
+				<label>Email Address</label>
+				<input>
+					{' '}
+					<div className="input-underline"></div>
+				</input>
+
+				<button>Create account</button>
+			</form>
+		</div>
+	);
 }
