162b5911276a85a2badd964402f4fc38711afa95
in-roma <romain.grandjean@outlook.com>
2020-09-27 22:43:25 +0200

    btn login and register connected

--- client/src/components/structure/Header.jsx
+++ client/src/components/structure/Header.jsx
@@ -1,59 +1,87 @@
 import React from 'react';
-import LoginRegister from '../login/LoginRegister';
+import Login from '../login/Login';
+import Register from '../login/Register';
+import ResetPassword from '../login/ResetPassword';
+
 import { Link, NavLink } from 'react-router-dom';
 
 class Header extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			display: 'nactive',
+			display: '',
+			window: '',
 		};
 	}
 	login = () => {
-		console.log('login fired');
-		this.setState({ display : 'active' });
-		
+		this.setState({ display: 'active', window: <Login /> });
+	};
 
-	} 
 	register = () => {
-		console.log('register fired');
-		this.setState({ display : 'active' });
-	}
+		this.setState({ display: 'active', window: <Register /> });
+	};
 
+	resetPassword = () => {
+		this.setState({ display: 'active', window: <ResetPassword /> });
+	};
 
 	render() {
 		return (
 			<>
-			<div className="header">
-				<Link to="" id="logo-header">
-					<img
-						src={process.env.PUBLIC_URL + '/img/logo_header.png'}
-						alt="logo"
-					/>
-				</Link>
+				<div className="header">
+					<Link to="" id="logo-header">
+						<img
+							src={
+								process.env.PUBLIC_URL + '/img/logo_header.png'
+							}
+							alt="logo"
+						/>
+					</Link>
 
-				<nav className="navigation">
-					<input type="checkbox" />
+					<nav className="navigation">
+						<input type="checkbox" />
 
-					<div className="line1"></div>
-					<div className="line2"></div>
+						<div className="line1"></div>
+						<div className="line2"></div>
 
-					<div className="nav1">
-						<NavLink to="" className="nav1-elements">Concept</NavLink>
-						<NavLink to="" className="nav1-elements">Contribute</NavLink>
-						<NavLink to="" className="nav1-elements">
-							For Schools & Recruiters
-						</NavLink>
-						<div className="line-nav"></div>
-					</div>
-					<div className="nav2">
-						<button className="nav2-elements" onClick={this.login}>Login</button>
-						<button className="button-signup" onClick={this.register}>Sign Up</button>
-					</div>
-				</nav>
-				<div className="line-header"></div>
-			</div>
-			<LoginRegister display={this.state.display}/>
+						<div className="nav1">
+							<NavLink to="" className="nav1-elements">
+								Concept
+							</NavLink>
+							<NavLink to="" className="nav1-elements">
+								Contribute
+							</NavLink>
+							<NavLink to="" className="nav1-elements">
+								For Schools & Recruiters
+							</NavLink>
+							<div className="line-nav"></div>
+						</div>
+						<div className="nav2">
+							<button
+								className="nav2-elements"
+								onClick={this.login}
+							>
+								Login
+							</button>
+							<button
+								className="button-signup"
+								onClick={this.register}
+							>
+								Sign Up
+							</button>
+						</div>
+					</nav>
+					<div className="line-header"></div>
+				</div>
+				<div
+					className="login-interface"
+					style={{
+						display:
+							this.state.display === 'active' ? 'flex' : 'none',
+					}}
+				>
+					{this.state.window}
+				</div>
 			</>
 		);
 	}
