b5b39c047fbe1b256072145a2b93ea0271ced05e
in-roma <romain.grandjean@outlook.com>
2020-09-27 12:59:25 +0200

    setting up reset password component

---
+++ client/src/components/login/ResetPassword.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function ResetPassword() {
+    return (
+        <div>
+            <form className="login-window">
+                <label>Email Address</label>
+                <input></input>
+                <button>Confirm</button>
+            </form>
+        </div>
+    )
+}
